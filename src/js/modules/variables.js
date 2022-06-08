dab060164e1458411aa730718b570bdc1ef306fe
Sorokovsky <Sorokovskys@ukr.net>
2022-06-08 14:23:30 +0300

    finish

--- src/js/modules/variables.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.menu = exports.burger = exports.header = void 0;
-exports.header = document.querySelector('.header'), exports.burger = document.querySelector('.burger'), exports.menu = document.querySelector('.menu');
